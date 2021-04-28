2fde7978c2ec95b55ca246667f7fc5e5904d64a9
Eobodoechine <79485467+Eobodoechine@users.noreply.github.com>
2021-04-27 22:55:54 -0400

    Delete api_keyz.py

--- WeatherPy/api_keyz.py
+++
@@ -1,7 +0,0 @@
-# OpenWeatherMap API Key
-
-yeehaw = "69f5907fa6ecb31671ff0108eaca1abd"
-#yeehaw = "5949a5166856ecc680f303e929f4917c"
-
-# Google API Key
-g_key = "AIzaSyDfhVlPKjWmO__D7Gx_Cem2y44gueth1sY"
